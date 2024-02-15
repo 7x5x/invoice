c6158d621c68a927af8a5e02bafa0b85ef3dbe61
ali al jabal <alialjabal4@gmail.com>
2024-02-15 16:03:25 +0300

    add swager th the project

--- src/temp.ts
+++ src/temp.ts
@@ -1,15 +1,22 @@
 import express, { Request, Response } from "express";
 import { main, tem } from "./index.js";
-import { send } from "process";
-import { ZATCAInvoiceLineItem } from "./zatca/templates/tax_invoice_template.js";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+// import swaggerConfig from "./swagger";
+const swagger = require("./swagger");
 
 const app = express();
 const port = 3000;
 app.use(express.json());
+// Initialize Swagger-jsdoc
+// const swaggerSpec = swaggerJsdoc(swaggerConfig);
+
+// Serve Swagger documentation
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 //deprecated
 app.use(express.urlencoded());
-
+swagger(app);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
@@ -20,8 +27,6 @@ app.get("/home", (req: Request, res: Response) => {
 app.post("/invoice", (req: Request, res: Response) => {
   const line_itemData = req.body;
   const line_item2 = tem(line_itemData);
- 
-  res.status(200).json(line_item2);
 
- 
+  res.status(200).json(line_item2);
 });
---
+++ swagger.ts
@@ -0,0 +1,19 @@
+// import { OpenAPIV3 } from "swagger-jsdoc";
+
+// const options: OpenAPIV3.Document = {
+//   openapi: "3.0.0",
+//   info: {
+//     title: "Express API with Swagger",
+//     version: "1.0.0",
+//     description: "API documentation using Swagger",
+//   },
+//   servers: [
+//     {
+//       url: "http://localhost:3000",
+//     },
+//   ],
+//   security: [{ bearerAuth: [] }],
+//   apis: ["./routes/*.ts"], // Path to your route files
+// };
+
+// export default options;
